a354fa81ecde73ffc8ece9af28d9dafe503102b0
komv8i <phani.komaravolu@cchmc.org>
2019-03-19 00:05:09 -0400

    remove unused packages and update help screen

--- cli.js
+++ cli.js
@@ -1,39 +1,34 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const meow = require('meow');
-const router = require('./src/router');
-const updateNotifier = require('update-notifier');
-const pkg = require('./package.json');
+const meow = require("meow");
+const router = require("./src/router");
+const updateNotifier = require("update-notifier");
+const pkg = require("./package.json");
 
 updateNotifier({ pkg }).notify();
 
-const cli = meow(`
+const cli = meow(
+  `
 Usage
 
-   $ yo-node-cli-skeleton <command> <params>
+   $ npm-downloads-graph-cli <command> <flag>
 
-   $ yo-node-cli-skeleton sample <param>             # Uses the <PARAM>
-   $ yo-node-cli-skeleton other <param>              # Other the <PARAM>
-   $ yo-node-cli-skeleton another <param>            # Another the <PARAM>
-   
  Examples
 
-   $ yo-node-cli-skeleton sample TEST                # Uses the TEST
-   $ yo-node-cli-skeleton sample YOLO                # Uses the YOLO
-   $ yo-node-cli-skeleton other YOLO                 # Uses the YOLO
-   $ yo-node-cli-skeleton another YOLO               # Uses the YOLO
+   $ npm-downloads-graph-cli ds                         # Get the download statistics
+   $ npm-downloads-graph-cli ds --lw                    # last week download statistics
 `,
   {
     alias: {
-      v: 'version'
+      v: "version"
     },
-    boolean: ['version']
+    boolean: ["version"]
   }
 );
 
 if (cli.input.length > 0) {
-	router.init(cli.input, cli.flags);
+  router.init(cli.input, cli.flags);
 } else {
-	cli.showHelp(2);
+  cli.showHelp(2);
 }
